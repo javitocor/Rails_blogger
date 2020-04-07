6594e732e25e3778cb5f313204e950671a04ff3f
javitocor <javitocor@gmail.com>
2020-04-07 16:43:06 +0200

    Added edit and delete functions to the website

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,5 +13,18 @@ class ArticlesController < ApplicationController
         @article = Article.new(article_params)
         @article.save    
         redirect_to article_path(@article)
-    end    
+    end   
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end 
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)      
+        redirect_to article_path(@article)
+    end
 end
